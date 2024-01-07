6bd986843183662d686321d6c285d209400db381
MDSadeepa <126347884+MDSadeepa@users.noreply.github.com>
2024-01-07 09:08:35 +0530

    first commit

---
+++ app/Models/Subcategory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subcategory extends Model
+{
+    use HasFactory;
+
+     // Relationship with products
+     public function products () {
+        return $this->hasMany(Product::class, 'subcategory_id');
+    }
+
+    // Relationship to Catagory
+    public function category () {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
